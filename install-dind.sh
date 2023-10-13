513179c486082c68727ed2b705b3c2c2caa4a6b6
idodod <idodod@gmail.com>
2023-10-13 15:28:51 -0400

    Update install-dind.sh
    use $distro instead of debian

--- install-dind.sh
+++ install-dind.sh
@@ -100,10 +100,10 @@ install_dockerd_debian_like() {
     apt_get_update
     apt-get install -y ca-certificates curl gnupg
     install -m 0755 -d /etc/apt/keyrings
-    curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+    curl -fsSL https://download.docker.com/linux/$distro/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
     chmod a+r /etc/apt/keyrings/docker.gpg
     echo \
-      "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
+      "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/$distro \
       "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
       tee /etc/apt/sources.list.d/docker.list > /dev/null
     apt-get update # dont use apt_get_update since we must update the newly added apt repo
